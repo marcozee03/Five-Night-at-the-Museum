5afe274b10456433e6d282ef247290d4b413c6ac
JLetsGitIt <86565950+JLetsGitIt@users.noreply.github.com>
2023-05-24 20:03:42 -0400

    Update PlayerBehavior.cs

--- Assets/PlayerBehavior.cs
+++ Assets/PlayerBehavior.cs
@@ -4,19 +4,55 @@ using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour
 {
+    AudioSource distractSFX;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        distractSFX = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
 
     }
+
+    void FixedUpdate()
+    {
+        if(!LevelManager.isGameOver)
+        {
+            if(Input.GetKeyDown(KeyCode.D))
+            {
+                if(LevelManager.distractions > 0)
+                {
+                    Distract();
+                }
+            }
+        }
+        else
+        {
+            // stops the player when the game is over
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+        }
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy"))
+        {
+            LevelManager lm = FindObjectOfType<LevelManager>();
+            lm.LevelLost();
+            Destroy(gameObject);
+        }
+    }
     
+    void Distract()
+    {
+        jumpSFX.Play();
+        //player throws item
+    }
     
 }
