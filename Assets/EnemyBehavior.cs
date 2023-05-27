4ee3b08f72275af8ecd93da86062777bf141fa08
JLetsGitIt <86565950+JLetsGitIt@users.noreply.github.com>
2023-05-26 21:05:54 -0400

    Update EnemyBehavior.cs

--- Assets/EnemyBehavior.cs
+++ Assets/EnemyBehavior.cs
@@ -9,11 +9,14 @@ public class EnemyBehavior : MonoBehaviour
     //public AudioClip enemySFX;
     Rigidbody rb;
     bool playerInView = false;
+    public float detectionRange;
+    bool isMoving;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        isMoving = false;
         if(player == null)
         {
             player = GameObject.FindGameObjectWithTag("Player").transform;
@@ -25,17 +28,61 @@ public class EnemyBehavior : MonoBehaviour
     {
         if(!LevelManager.isGameOver)
         {
+            Detected();
             if(playerInView)
             {
                 transform.LookAt(player);
                 transform.position = Vector3.MoveTowards
-                (transform.position, player.position, moveSpeed * Time.deltaTime);
+                    (transform.position, player.position, moveSpeed * Time.deltaTime);
+                isMoving = true;
+                //enemySFX.Play();
             }
             else
             {
                 //implement random behavior for patrolling
+                moveRandom();
             }
-            
+        }
+    }
+
+    bool Detected()
+    {
+        float distance = Vector3.Distance(transform.position, player.position);
+        if(distance < detectionRange)
+        {
+            Vector3 playerDirection = transform.position - player.position;
+            float angle = Vector3.Angle(transform.forward, playerDirection);
+            if(Mathf.Abs(angle) > 90 && Mathf.Abs(angle) < 270)
+            {
+                //playerInView = true;
+            }
+            RaycastHit hit;
+            if(Physics.Raycast(transform.position, transform.forward, out hit, range))
+            {
+                if(hit.transform.name == "Player")
+                {
+                    //playerInView = true;
+                }
+            }
+        }
+    }
+
+    void moveRandom()
+    {
+        GameObject[] randomSpots = FindGameObjectsWithTag("Random");
+        //GameObject[] listOfRandom = randomSpots; //does this change original list?
+        int index = 0;
+        if(!isMoving)
+        {
+            isMoving = true;
+            GameObject thisSpot = randomSpots[index];
+            transform.position = Vector3.MoveTowards
+                    (transform.position, thisSpot.transform.position, moveSpeed * Time.deltaTime);
+            index++;
+            if(index == randomSpots.Length)
+            {
+                index = 0;
+            }
         }
     }
 }
