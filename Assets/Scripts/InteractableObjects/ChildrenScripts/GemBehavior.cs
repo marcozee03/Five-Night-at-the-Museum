ee6fe621c11b8e8f429242aa3be52adddbac7d0d
frank <frankatank1@gmail.com>
2023-05-31 19:31:54 -0700

    added gem pick up sound

--- Assets/Scripts/InteractableObjects/ChildrenScripts/GemBehavior.cs
+++ Assets/Scripts/InteractableObjects/ChildrenScripts/GemBehavior.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class GemBehavior : InteractableObject
 {
     public int Value = 100;
+    public AudioClip pointsSFX;
+   
     public override void Interact(PlayerController player)
     {
+        AudioSource.PlayClipAtPoint(pointsSFX, Camera.main.transform.position);
         HUDManager.points += Value;
         Destroy(gameObject);
+        
+        
     }
     override public string HoverTextMnK()
     {
