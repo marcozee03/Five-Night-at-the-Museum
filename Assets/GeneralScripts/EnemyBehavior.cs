f3b1a720ab1f685d34aab1487d58cb969ca6b665
EmzyShmemzy <123316474+EmzyShmemzy@users.noreply.github.com>
2023-06-06 02:20:25 -0400

    Fix?

---
+++ Assets/GeneralScripts/EnemyBehavior.cs
@@ -0,0 +1,129 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    public Transform player;
+    public float moveSpeed = 2;
+    //public AudioClip enemySFX;
+    Rigidbody rb;
+    bool playerInView = false;
+    public float detectionRange = 5;
+    bool isMoving;
+    UnityEngine.AI.NavMeshAgent myNavMeshAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        isMoving = false;
+        if (player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+        myNavMeshAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!HUDManager.isGameOver)
+        {
+            Detected();
+            if (playerInView)
+            {
+                float distance = Vector3.Distance(transform.position, player.position);
+                Debug.Log("player in view");
+                transform.LookAt(player);
+                //transform.position = Vector3.MoveTowards
+                //    (transform.position, player.position, moveSpeed * Time.deltaTime);
+                myNavMeshAgent.SetDestination(player.position);
+                isMoving = true;
+                moveSpeed *= 3;
+                gameObject.GetComponent<Animator>().SetInteger("animState", 1);
+                if (PlayerBehavior.hiding)
+                {
+                    isMoving = false;
+                    playerInView = false;
+                    //Debug.Log("continue patrol");
+                    moveRandom();
+                }
+                // else if(distance > detectionRange)
+                // {
+                //     playerInView = false;
+                //     moveSpeed /= 3;
+                // }
+                //enemySFX.Play(); chase music
+            }
+            //else if (HUDManager.distracted)
+            //{
+            //    myNavMeshAgent.SetDestination(HUDManager.distractLoc);
+            //    Debug.Log("distracted: " + transform.position + " going to " + HUDManager.distractLoc);
+            //    isMoving = true;
+            //    float distance = Vector3.Distance(transform.position, HUDManager.distractLoc);
+            //    if (distance < 2)
+            //    {
+            //        isMoving = false;
+            //        HUDManager.distracted = false;
+            //        Debug.Log("continue patrol");
+            //        moveRandom();
+            //    }
+            //}
+            else
+            {
+                //implement random behavior for patrolling
+                moveRandom();
+            }
+        }
+    }
+
+    void Detected()
+    {
+        float distance = Vector3.Distance(transform.position, player.position);
+        if (distance < detectionRange)
+        {
+            Vector3 playerDirection = transform.position - player.position;
+            float angle = Vector3.Angle(transform.forward, playerDirection);
+            if (Mathf.Abs(angle) > 90 && Mathf.Abs(angle) < 270)
+            {
+                playerInView = true;
+            }
+            RaycastHit hit;
+            playerInView = true;
+            if (Physics.Raycast(transform.position, transform.forward, out hit, detectionRange))
+            {
+                if (hit.collider.gameObject.CompareTag("Player"))
+                {
+                    playerInView = true;
+                }
+            }
+        }
+    }
+
+    int index = 0;
+    void moveRandom()
+    {
+        gameObject.GetComponent<Animator>().SetInteger("animState", 2);
+        GameObject[] randomSpots = GameObject.FindGameObjectsWithTag("Random");
+        if (!isMoving)
+        {
+            GameObject thisSpot = randomSpots[index];
+            if (transform.position.x != thisSpot.transform.position.x || transform.position.z != thisSpot.transform.position.z)
+            {
+                // transform.position = Vector3.MoveTowards
+                //     (transform.position, thisSpot.transform.position, moveSpeed * Time.deltaTime);
+                myNavMeshAgent.SetDestination(thisSpot.transform.position);
+
+            }
+            else
+            {
+                index++;
+            }
+            if (index == randomSpots.Length)
+            {
+                index = 0;
+            }
+        }
+    }
+}
---
+++ Assets/Player/PlayerController.cs
@@ -0,0 +1,218 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    #region Adjustable Stats
+    public float airAcceleration;
+    public float acceleration;
+    public float deceleration;
+    public float walkSpeed;
+    public float sprintSpeed;
+    public float jumpHeight;
+    public float gravity;
+    public float maxStamina;
+    public float staminaRecoveryRate;
+    public float staminaLossRate;
+
+    [Tooltip("how long it takes after they stop sprinting to start recovering stamina")]
+    public float RecoveryCooldown;
+    #endregion
+
+    #region Internal References
+    private bool sprinting = false;
+    public bool DisableMovement;
+    public void EnableMovement()
+    {
+        DisableMovement = false;
+    }
+    public float CurrentStamina { get; private set; }
+    private Vector3 targetVelocity; // the speed the player will be moving at the end of this frame;
+    Vector3 Speed => controller.velocity;
+    float LateralSpeed => Mathf.Sqrt(Mathf.Pow(controller.velocity.x, 2) + Mathf.Pow(controller.velocity.z, 2)); // the current speed combining the X and Z components
+    public CharacterController controller;
+    private void OnValidate()
+    {
+        if(controller == null)
+        {
+            Debug.LogWarning("Please Assign CharacterController to field");
+        }
+    }
+
+    private void Start()
+    {
+        CurrentStamina = maxStamina;
+        CurrentStamina = maxStamina;
+    }
+    private void Update()
+    {
+            HandleHorizontal();
+            HandleVertical();
+            HandleStamina();
+        controller.Move(velocity * Time.deltaTime);
+    }
+    #endregion
+
+    #region MovementHandling
+    private Vector3 velocity;
+    float AHI, AVI; //Adjusted Vertical/Horizontal Input
+    private void HandleHorizontal()
+    {
+        float rotation = -Camera.main.gameObject.transform.eulerAngles.y;
+        AHI = horizontalInput * Mathf.Cos(Mathf.Deg2Rad * rotation) - verticalInput * Mathf.Sin(Mathf.Deg2Rad * rotation);
+        AVI = horizontalInput * Mathf.Sin(Mathf.Deg2Rad * rotation) + verticalInput * Mathf.Cos(Mathf.Deg2Rad * rotation);
+        float x, z;
+        float targetSpeed = CanSprint ? sprintSpeed : walkSpeed;
+        if (Mathf.Abs(AHI) > .05)
+        {
+            x = Mathf.MoveTowards(velocity.x, targetSpeed * AHI, acceleration * Time.deltaTime);
+        }
+        else
+        {
+            x = Mathf.MoveTowards(velocity.x, 0, deceleration * Time.deltaTime);
+        }
+        if (Mathf.Abs(AVI) > .05)
+        {
+            z = Mathf.MoveTowards(velocity.z, targetSpeed * AVI, acceleration * Time.deltaTime);
+        }
+        else
+        {
+            z = Mathf.MoveTowards(velocity.z, 0, deceleration * Time.deltaTime);
+        }
+        velocity = new Vector3(x, velocity.y, z);
+    }
+    private void HandleVertical()
+    {
+        velocity.y = Speed.y;
+        velocity.y -= gravity * Time.deltaTime;
+        if (CanJump)
+        {
+            velocity.y = Mathf.Sqrt(2 * jumpHeight * gravity);
+        }
+    }
+    #endregion
+
+    #region Collision
+    [Min(0)] public float GroundDistance = .1f;
+    private bool GroundCheck()
+    {
+        return Physics.SphereCast(transform.position, controller.radius, transform.up * -1, out RaycastHit hitInfo, GroundDistance);
+    }
+    #endregion
+
+    #region Stamina
+    private float TimeExhaustedStamina = float.MinValue;
+    private void HandleStamina()
+    {
+        if (CanSprint && (new Vector2(horizontalInput, verticalInput).magnitude >= walkSpeed / sprintSpeed))      
+        {
+            ReduceStamina(staminaLossRate * Time.deltaTime);
+        }
+        else
+        {
+            ReplenishStamina(staminaRecoveryRate * Time.deltaTime);
+        }
+    }
+    public void ReplenishStamina(float amount)
+    {
+        CurrentStamina += amount;
+        CurrentStamina = Mathf.Clamp(CurrentStamina, 0, maxStamina);
+
+    }
+
+    private bool CanSprint => sprinting && !Exhausted;
+    public bool Exhausted => Time.time - TimeExhaustedStamina < RecoveryCooldown;
+
+    public void ReduceStamina(float amount)
+    {
+        CurrentStamina -= amount;
+        CurrentStamina = Mathf.Clamp(CurrentStamina, 0, maxStamina);
+        if (CurrentStamina == 0 && !Exhausted)
+        {
+            TimeExhaustedStamina = Time.time;
+        }
+    }
+    #endregion
+
+    #region interact
+    public float maxObjectDistance = 1;
+
+    private bool HoveredIsInteractble(RaycastHit hovered, out InteractableObject obj)
+    {
+        obj = hovered.collider.gameObject.GetComponent<InteractableObject>();
+        return obj != null;
+    }
+    private void HoverInteractableObject()
+    {
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out RaycastHit hitInfo, maxObjectDistance) && HoveredIsInteractble(hitInfo, out InteractableObject obj))
+        {
+            if (obj != null)
+            {
+                GameObject.FindFirstObjectByType<HUDManager>().SetHoverText(obj.HoverTextMnK());
+            }
+        }
+        else
+        {
+            GameObject.FindFirstObjectByType<HUDManager>().SetHoverText("");
+        }
+    }
+    public void Interact(InputAction.CallbackContext context)
+    {
+        //Debug.Log("context given" + context.started);
+        if (!context.started) return;
+        string debugString = "Interact Pressed";
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out RaycastHit hitInfo, maxObjectDistance))
+        {
+            debugString += " Ray collided";
+            InteractableObject obj = hitInfo.collider.gameObject.GetComponent<InteractableObject>();
+            if (obj != null)
+            {
+                debugString += " found interactableObject";
+                obj.Interact(GetComponent<PlayerController>());
+            }
+        }
+        Debug.Log(debugString);
+    }
+    #endregion
+
+    #region Unity InputSystem Events
+    private float horizontalInput;
+    private float verticalInput;
+    public void LogMoveInput(InputAction.CallbackContext context)
+    {
+        Vector2 input = context.ReadValue<Vector2>();
+        horizontalInput = input.x;
+        verticalInput = input.y;
+    }
+
+    bool jumpPressed = false;
+    bool jumpHeld = false;
+    private float timeJumpWasPressed = float.MinValue;
+    public float JumpBuffer = .5f;
+    private float TimeSinceJumpWasPressed => Time.time - timeJumpWasPressed;
+    private bool CanJump => GroundCheck() && TimeSinceJumpWasPressed <= JumpBuffer;
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            timeJumpWasPressed = Time.time;
+        }
+        jumpPressed = context.started;
+        jumpHeld = context.performed || jumpPressed;
+    }
+
+    public void TriggerSprint(InputAction.CallbackContext context)
+    {
+        sprinting = !context.canceled;
+    }
+    #endregion
+
+    #region Gizmos
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position - (transform.up * GroundDistance), controller.radius);
+    }
+    #endregion
+}
